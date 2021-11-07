10cb1238b5fb4407dfc22596535736bb1cf113f6
anika-001 <dream.sweeter.30.09@gmail.com>
2021-11-07 23:08:09 +0530

    mentalescape

---
+++ src/app/JSONData/navbar.ts
@@ -0,0 +1,46 @@
+export let navbar = [
+    {
+        "role":"user",
+        "name":"Mood Tracker",
+        "link":"/dashboard",
+    },
+    {
+        "role":"user",
+        "name":"Forum",
+        "link":"/forum",
+    },
+    // {
+    //     "role":"user",
+    //     "name":"Translate via Voice",
+    //     "link":"/signt",
+    // },{
+    //     "role":"user",
+    //     "name":"Translate via Text",
+    //     "link":"/texttrans",
+    // },
+    {
+        "role":"user",
+        "name":"helpful Articles",
+        "link":"/news",
+    },
+    {
+        "role":"user",
+        "name":"Home",
+        "link":"/home",},
+    // },
+    {
+        "role":"user",
+        "name":"Calmness",
+        "link":"/beach",
+    },
+    // {
+    //     "role":"user",
+    //     "name":"calendar",
+    //     "link":"/management",
+    // },
+    {
+        "role":"user",
+        "name":"events",
+        "link":"/events",
+    }
+]
